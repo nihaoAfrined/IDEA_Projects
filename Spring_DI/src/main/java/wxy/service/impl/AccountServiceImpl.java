4b5ca25e6539422681aae6bca2e8f407868065c6
nihaoAfrined <53180430+nihaoAfrined@users.noreply.github.com>
2020-12-30 15:30:52 +0800

    添加到仓库

---
+++ Spring_DI/src/main/java/wxy/service/impl/AccountServiceImpl.java
@@ -0,0 +1,20 @@
+package wxy.service.impl;
+
+import wxy.dao.IAccountDao;
+import wxy.dao.impl.AccountDaoImpl;
+import wxy.service.IAccountService;
+
+import java.util.Date;
+
+public class AccountServiceImpl implements IAccountService {
+
+
+    public void saveAccount(){
+
+        private String name;
+        private Integer age;
+        private Date birthday;
+
+        System.out.println("service中的saveAccount方法执行了。。。");
+    }
+}
